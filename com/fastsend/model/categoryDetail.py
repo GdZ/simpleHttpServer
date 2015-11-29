7796ec999edf2a70957dcb18fa9abe354e721bb2
M.R.Z <zgd_521@yahoo.com>
2015-11-29 04:08:30 +0100

    add model

---
+++ com/fastsend/model/categoryDetail.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+__author__ = 'self'
+
+def getCategoryDetail():
+    imgList = []
+    for i in range(0, 4, 1):
+        img = {}
+        img['url'] = 'http://xxoo/' + str(i) + '.jpg'
+        img['to'] = 'activityDetail'
+        img['title'] = '图标说明-' + str(i)
+        imgList.append(img)
+    itemList = []
+    for j in range(0, 6, 1):
+        item = {}
+        item['index'] = j
+        item['List'] = imgList
+        itemList.append(item)
+    return itemList
